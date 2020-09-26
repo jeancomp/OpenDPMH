343f72644ed9c3e81e48a8acf537df189967e6be
jeancomp <jeancomp@gmail.com>
2020-09-26 11:33:40 -0300

    Commit: Adicionei a jar com a parte do server-ivan

--- app/src/main/java/br/lsdi/ufma/digitalphenotyping/model/ContextAttributeEntity.java
+++ app/src/main/java/br/lsdi/ufma/digitalphenotyping/model/ContextAttributeEntity.java
@@ -1,22 +1,25 @@
 package br.lsdi.ufma.digitalphenotyping.model;
 
-import com.lsdi.social.mhealth.model.ContextAttribute;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+//import com.lsdi.social.mhealth.model.ContextAttribute;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
 
-import javax.persistence.*;
+//import javax.persistence.*;
+
+import com.lsdi.social.mhealth.model.ContextAttribute;
+
 import java.io.Serializable;
 
 
-@Entity
-@Table(name = "CONTEXT_ATTRIBUTE")
-@Getter @Setter
-@NoArgsConstructor
+//@Entity
+//@Table(name = "CONTEXT_ATTRIBUTE")
+//@Getter @Setter
+//@NoArgsConstructor
 public class ContextAttributeEntity implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name ="context_attribute_id")
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name ="context_attribute_id")
     private long id;
     private String label;
     private String description;
@@ -29,5 +32,35 @@ public class ContextAttributeEntity implements Serializable {
         this.contextType = contextAttribute.getContextType();
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContextType() {
+        return contextType;
+    }
+
+    public void setContextType(String contextType) {
+        this.contextType = contextType;
+    }
 }
